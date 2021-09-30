c817139a4d18ef5c1d13f57f450a9b83255e081a
”sovsianykov” <swebproger@gmail.com>
2021-09-30 14:50:59 +0300

    heroku uploaded2

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export const BASE_URL = "/api/items";
+export const BASE_URL = "https://shoppinglist-new.herokuapp.com/api/items";
 export const useFetch =  (url) => {
   const [response, setResponse] = useState({});
   const [isLoading, setIsLoading] = useState(true);
