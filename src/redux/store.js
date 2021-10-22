796e7fc21f5f5c4c6300a06fcb60b8b37b472ba2
”sovsianykov” <swebproger@gmail.com>
2021-10-23 01:07:02 +0300

    date 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import thunk from "redux-thunk";
 const rootReducer = combineReducers({
     orderReducer
 })
-const store = createStore(rootReducer, compose( applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose( applyMiddleware(thunk)
+    ))
 
 export default store
